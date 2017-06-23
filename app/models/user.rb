aec0b922bbedad42ed3225b9e0c21ed5df7df6da
劉沂寬 <ga771358@gmail.com>
2017-06-24 01:12:09 +0800

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+    validates :sid, presence: true
+    validates :email, presence: true
+    validates :tnum, presence: true
+    validates :score, presence: true
+    validates_uniqueness_of :email
+    validates_uniqueness_of :sid
 end

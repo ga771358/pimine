45b225c29f61766b38c09a195b40da097ffc9243
Kira <leedj2718@gmail.com>
2017-06-23 21:03:09 +0800

    add post new.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,19 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.save
+
+		redirect_to posts_path
+	end
+
+	def post_params
+		params.require(:post).permit(:user_id,:title,:type,:price,:tclick,:location,:content,:is_sold)
+	end
 end

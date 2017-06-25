96af449a62df820696172802c6d39f4184cb0234
劉沂寬 <ga771358@gmail.com>
2017-06-25 13:09:44 +0800

    show buyer name and mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_many :posts
+    has_many :userposts
     validates :name, presence: true
     validates :sid, presence: true
     validates :email, presence: true
--- app/models/userpost.rb
+++ app/models/userpost.rb
@@ -1,4 +1,5 @@
 class Userpost < ApplicationRecord
+    belongs_to :user
     has_many :userpost_postships
     has_many :posts, :through => :userpost_postships
 end
